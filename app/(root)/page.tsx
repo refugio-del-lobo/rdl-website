61246e3457bfde001e8262a8b9d56ed046cc8a0f
fede-blanco <info@refugiodellobo.com>
2024-04-17 19:24:01 -0300

    cambio tamaño de video institucional a mas pequeño

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,6 +2,8 @@ import { Button } from "@/components/ui/button"
 import Image from "next/image"
 import Link from "next/link"
 
+
+
 export default function Home() {
   return (
     <div className=" bg-casa-lejos-bg bg-center">
@@ -11,7 +13,10 @@ export default function Home() {
             Disfrutá la patagonia Argentina
           </h1>
           <div className=" flex flex-col text-right w-full px-2 tracking-wide drop-shadow-md hero-text">
-            <p className="w-full md:w-9/12 self-end">Te invitamos a vivir experiencias inolvidables, paisajes impresionantes y una gastronomía con un toque hogareño</p>
+            <p className="w-full md:w-9/12 self-end">
+              Te invitamos a vivir experiencias inolvidables, paisajes
+              impresionantes y una gastronomía con un toque hogareño
+            </p>
           </div>
         </div>
 
@@ -23,16 +28,28 @@ export default function Home() {
               muted
               loop
               playsInline
-              controls
+              // controls
               preload="true"
               className="video"
             ></video>
+            {/* <video
+              src="assets/videos/rdl_reel_camino_a_chapelco.mp4"
+              autoPlay
+              muted
+              loop
+              playsInline
+              // controls
+              preload="true"
+              className="video"
+              // id="miVideo"
+              // ref={videoRef}
+              // onLoadedMetadata={handleLoadedMetadata}
+            ></video> */}
           </div>
         </div>
 
         <div className="wrapper flex flex-col gap-6 text-3xl text-center">
           <div className="flex flex-wrap justify-around opacity-layer-90 rounded-md">
-            
             <div className="flex-col justify-between text-2xl flex w-full md:w-6/12 p-6">
               <div>
                 <div className="flex flex-col">
@@ -54,11 +71,10 @@ export default function Home() {
                         />
                       </svg>
                       <p>
-
-                      Wifi{" "}
-                      <span className="text-lg text-gray-500">
-                        {"("}150 megas c/ fibra óptica{")"}
-                      </span>
+                        Wifi{" "}
+                        <span className="text-lg text-gray-500">
+                          {"("}150 megas c/ fibra óptica{")"}
+                        </span>
                       </p>
                     </li>
                     <li className="flex items-center gap-2">
@@ -101,8 +117,8 @@ export default function Home() {
                       </svg>
                       <p>Open-bar</p>
                       <span className="text-lg text-gray-500">
-                          {"("}Self service{")"}
-                        </span>
+                        {"("}Self service{")"}
+                      </span>
                     </li>
                     <li className="flex items-center gap-2">
                       <svg
@@ -139,12 +155,11 @@ export default function Home() {
                       <p>
                         Gestión de transfer privado{" "}
                         <span className="text-lg text-gray-500">
-                          {"("}in-out Aeropuerto o terminal de ómnibus / Cerro Chapelco / Otros{")"}
+                          {"("}in-out Aeropuerto o terminal de ómnibus / Cerro
+                          Chapelco / Otros{")"}
                         </span>
                       </p>
                     </li>
-
-
                   </ul>
                 </div>
               </div>
@@ -185,9 +200,9 @@ export default function Home() {
                     className="thumb-img"
                   />
                   <Link href={"/instalaciones"}>
-                  <div className="thumb-img bg-[#ffffffc2] scale-75 flex justify-center items-center text-gray-500">
-                    Ver mas...
-                  </div>
+                    <div className="thumb-img bg-[#ffffffc2] scale-75 flex justify-center items-center text-gray-500">
+                      Ver mas...
+                    </div>
                   </Link>
                 </div>
               </div>
@@ -198,7 +213,7 @@ export default function Home() {
                 <div className="flex flex-col">
                   <h3 className="text-4xl mb-4">Actividades</h3>
                   <ul className="flex flex-col gap-2 text-start">
-                  <li className="flex items-center gap-2">
+                    <li className="flex items-center gap-2">
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
@@ -248,9 +263,7 @@ export default function Home() {
                           d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
                         />
                       </svg>
-                      <p>
-                        Masajes
-                      </p>
+                      <p>Masajes</p>
                     </li>
                     <li className="flex items-center gap-2">
                       <svg
@@ -267,9 +280,7 @@ export default function Home() {
                           d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
                         />
                       </svg>
-                      <p>
-                        Cabalgatas
-                      </p>
+                      <p>Cabalgatas</p>
                     </li>
                     <li className="flex items-center gap-2">
                       <svg
@@ -342,7 +353,7 @@ export default function Home() {
                     </li>
                   </ul>
                   <p className="mt-2 text-base text-gray-500">
-                  Es esencial organizar las excursiones con tiempo
+                    Es esencial organizar las excursiones con tiempo
                   </p>
                 </div>
               </div>
@@ -383,9 +394,9 @@ export default function Home() {
                     className="thumb-img"
                   />
                   <Link href={"/instalaciones"}>
-                  <div className="thumb-img bg-[#ffffffc2] scale-75 flex justify-center items-center text-gray-500">
-                    Ver mas...
-                  </div>
+                    <div className="thumb-img bg-[#ffffffc2] scale-75 flex justify-center items-center text-gray-500">
+                      Ver mas...
+                    </div>
                   </Link>
                 </div>
               </div>
