c5dc23f70e5f56fd1830277446df8985ed4081e0
fede-blanco <info@refugiodellobo.com>
2024-04-18 09:58:17 -0300

    Add public/assets/images/casa_de_lejos.jpg

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -67,7 +67,7 @@ const SheetContent = React.forwardRef<
     >
       {children}
       <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none disabled:pointer-events-none data-[state=open]:bg-secondary">
-        <X className="h-10 w-10" />
+        <X className="h-10 w-10 text-black" />
         <span className="sr-only">Close</span>
       </SheetPrimitive.Close>
     </SheetPrimitive.Content>
